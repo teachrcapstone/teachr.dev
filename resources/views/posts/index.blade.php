7fe411da8689c77bfadb52607039a695438896c1
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-09-05 10:06:39 -0500

    created posts index view

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title')
+	<title>Lesson Plans</title>
+@stop
+
+@section('content')
+	
+	<main class="container">
+		<div class="row">
+			
+		</div>
+	</main>
+@stop
