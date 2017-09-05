0dc8e2d84cb512e2107b6912a840b649ae2576b5
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-09-05 10:02:58 -0500

    created view to submit lesson plan

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('title')
+	<title>Create a Lesson Plan</title>
+@stop
+
+@section('content')
+	
+	<main class="container">
+		<h1>Create a Lesson Plan</h1>
+		<form method="POST" action="{{action('PostsController@store')}}">
+			{!! csrf_field() !!}
+
+			<input class="form-control" type="text" name="title" placeholder="Lesson Name..." value="{{old('title')}}">
+
+			<textarea class="form-control" type="textarea" name="content" rows="4" cols="20"> {{old('content')}} </textarea>
+
+			<p> placeholder for file upload</p>
+
+			{{ method_field('POST')}}
+
+			<button class="btn-success btn" type="submit">Submit for Grading!</button>
+	</main>
+
+@stop
