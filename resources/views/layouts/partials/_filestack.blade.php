e92b50f564e4de24f382282fe5086344198f6736
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-09-06 08:53:18 -0500

    fully realized partial for filestack

--- resources/views/layouts/partials/_filestack.blade.php
+++ resources/views/layouts/partials/_filestack.blade.php
@@ -7,12 +7,15 @@
         	client.pick({
         		accept: "image/*",
         		maxFiles: 1,
-        		fromSources: ['local_file_system', 'facebook', 'googledrive', 'webcam'],	
+        		fromSources: ['local_file_system', 'facebook', 'googledrive', 'webcam'],
         		maxSize: 400*400,
         	}).then(function(result){
         		console.log(JSON.stringify(result));
         		var handle = result.filesUploaded[0].handle;
         		console.log(handle);
+
+        		var imageHandle = "<input type='hidden' value='"+handle+"' name='imageHandle'></input>"
+        		$("#image").append(imageHandle);
         	});
         }
 </script>
