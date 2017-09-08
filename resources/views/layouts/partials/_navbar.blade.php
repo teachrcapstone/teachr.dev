4fd51eca035cf1b53c4e5e489a4c437eb5307b71
Vivian <vcnales@gmail.com>
2017-09-08 13:41:30 -0500

    Navbar fix - added account option

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -27,7 +27,15 @@
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="{{ action('Auth\AuthController@getLogout') }}"">Logout</a></li>
+					<!-- <li><a href="{{ action('Auth\AuthController@getLogout') }}"">Logout</a></li> -->
+
+					<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Account Information</a></li>
+							<li><a href="{{ action('Auth\AuthController@getLogout') }}">Logout</a></li>
+						</ul>
+					</li>
 				</ul>
 
 			@else
