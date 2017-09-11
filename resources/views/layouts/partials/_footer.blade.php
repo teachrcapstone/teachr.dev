ce671e7dfe625fc6088ca2a897bb7047b1e243a6
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-09-11 13:44:31 -0500

    footer now sticks to bottom

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="navbar-default fixed-bottom">
+<footer class="navbar-default navbar-fixed-bottom">
 	<div class="row">
 		<p class="text-center">© 2017 Teachr, Inc.</p>
 		<a href="https://github.com/teachrcapstone/teachr.dev"><p class="text-center">Check us out on GitHub!</p></a>
