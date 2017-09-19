8dc9fab786085a6439a754398279faadb969e36a
Vivian <vcnales@gmail.com>
2017-09-19 12:02:39 -0500

    syle change

--- resources/views/posts/userposts.blade.php
+++ resources/views/posts/userposts.blade.php
@@ -14,9 +14,11 @@
 			<h2>My Posts</h2>
 			<h4>Total Results: {{ $userPosts->total() }}</h4>
 				@foreach($userPosts as $post)
-					<a href="{{action('PostsController@show', $post->id)}}"><h3>{{$post->title}}</h3></a>
+					<h3>
+						<a href="{{action('PostsController@show', $post->id)}}">{{$post->title}}</a>
+						<p><small>Created: {{$post->created_at}}</small></p>
+					</h3>
 					<p>{{$post->content }}</p>
-					<p>Created: {{$post->created_at}}</p>
 
 					<hr>
 				@endforeach
